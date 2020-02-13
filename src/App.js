19e465668b76efef81454d1f089be68c816a0f90
winkleman <jwinks18@gmail.com>
2020-02-13 08:54:14 -0600

    last

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Navigation from './components/Navigation'
 import './App.css'
+import Router from './Router'
+import { BrowserRouter } from 'react-router-dom';
+// import Home from './components/Home'
+// import About from './components/About'
 // Write imports for Router & BrowserRouter here //
 
 function App() {
   return (
-    <Navigation />
+    <BrowserRouter>
+      <Navigation />
+      <Router/>
+    </BrowserRouter>
   );
 }
 
